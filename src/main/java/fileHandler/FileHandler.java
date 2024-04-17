41a52c53b5d3b709183960b23ad678a23504bb6d
vasilije.pecanin <Vasilije.Pecanin@htlstp.at>
2024-04-17 13:56:08 +0200

    file handling added

--- src/main/java/fileHandler/FileHandler.java
+++ src/main/java/fileHandler/FileHandler.java
@@ -4,4 +4,8 @@ public class FileHandler {
     public String readFile(String filename) {
         return filename;
     }
+
+    public void writeFile(String filename, String content) {
+        System.out.println("Writing to file: " + filename);
+    }
 }
