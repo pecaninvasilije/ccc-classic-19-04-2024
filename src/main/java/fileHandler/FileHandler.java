ff510b8ff96431bf905a4ededba299fc9cba8ac5
vasilije.pecanin <Vasilije.Pecanin@htlstp.at>
2024-04-17 14:11:07 +0200

    FileHandler finished

--- src/main/java/fileHandler/FileHandler.java
+++ src/main/java/fileHandler/FileHandler.java
@@ -1,11 +1,38 @@
 package fileHandler;
 
+import java.io.*;
+
 public class FileHandler {
-    public String readFile(String filename) {
-        return filename;
+    private String filename;
+
+    public FileHandler(String filename) {
+        this.filename = filename;
     }
 
-    public void writeFile(String filename, String content) {
-        System.out.println("Writing to file: " + filename);
+    public String readFile() {
+        StringBuilder content = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line);
+                content.append("\n");
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return content.toString();
+    }
+
+    public void writeFile(String content) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+            writer.write(content);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
     }
 }
